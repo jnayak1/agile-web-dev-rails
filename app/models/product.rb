c92695c59fe5d4e76e29ba3be7c04d5342bdc1f0
Jesse Nayak <jdn4ae@virgina.edu>
2018-04-04 14:01:04 -0400

    Add add to cart button with tests that ensure proper redirect.

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,11 +13,13 @@ class Product < ApplicationRecord
 		too_short: "%{count} character is the minimum required" }
 
 
-	private
-		def ensure_not_referenced_by_any_line_item
-			unless line_items.empty?
-				errors.add(:base, 'Line Items present')
-				throw :abort
-			end
-		end
+  private
+
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+	  unless line_items.empty?
+	    errors.add(:base, 'Line Items present')
+	    throw :abort
+	  end
+	end
 end
