b7815fd0017a378aa23cee10539955f154d016ea
Jesse Nayak <jdn4ae@virgina.edu>
2018-04-04 13:19:07 -0400

    Scaffold line items and tests to ensure proper behavior on deletions.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -49,6 +49,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   # ...
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -56,5 +65,5 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
-  
+
 end
